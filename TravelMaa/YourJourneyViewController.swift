7e0042e8f938dbeb1892d60524a7d071038ee816
Rohit Gurnani <rkgi10@gmail.com>
2015-04-12 09:57:45 +0530

    awesome splash image added

--- TravelMaa/YourJourneyViewController.swift
+++ TravelMaa/YourJourneyViewController.swift
@@ -12,6 +12,13 @@ class YourJourneyViewController: UITableViewController {
     var Tripname = ""
 
     
+    @IBOutlet weak var dateLabelstart: UIDatePicker!
+    
+    
+    @IBOutlet var datePickerend: UIDatePicker!
+    
+    
+    
     @IBAction func NextPressedSaveData(sender: UIButton) {
         println("Data Saved")
         var defaults = NSUserDefaults.standardUserDefaults()
@@ -19,9 +26,9 @@ class YourJourneyViewController: UITableViewController {
         
         defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
         defaults.setObject(LocationFeild.text, forKey: "MyLocationName")
-        defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
-        defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
-        defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
+        defaults.setObject(TravelModeFeild.text, forKey: "MyTravelModepName")
+        defaults.setObject(BusinessEvents.text, forKey: "MyBusinessEventsName")
+        defaults.setObject(SocialEvents.text, forKey: "MySocialeventsName")
         defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
         defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
         defaults.setObject(TripNameFeild.text, forKey: "MyTripName")
@@ -37,11 +44,15 @@ class YourJourneyViewController: UITableViewController {
     
     @IBOutlet weak var StartDateFeild: UITextField!
     
-    @IBOutlet weak var EndDateFeild: UITextField!
+    
+    @IBOutlet var ArrivalTimePicker: UIDatePicker!
+    @IBOutlet var DepartureTimePicker: UIDatePicker!
+    @IBOutlet var EndDateFeild: UITextField!
     @IBOutlet weak var SocialStepperOutlet: UIStepper!
     @IBOutlet weak var DepartureFeild: UITextField!
-    @IBOutlet weak var TravelModeFeild: UITextField!
     
+    
+    @IBOutlet var TravelModeFeild: UITextField!
     
     
     
@@ -62,7 +73,56 @@ class YourJourneyViewController: UITableViewController {
         SocialStepperOutlet.wraps = true
         SocialStepperOutlet.autorepeat = true
         SocialStepperOutlet.maximumValue = 10
+        
+        dateLabelstart.addTarget(self, action: Selector("datePickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        datePickerend.addTarget(self, action: Selector("datePickerChanged2:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        ArrivalTimePicker.addTarget(self, action: Selector("timePickerChanged1:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        DepartureTimePicker.addTarget(self, action: Selector("timePickerChanged2:"), forControlEvents: UIControlEvents.ValueChanged)
     }
+    
+    func datePickerChanged(dateLabelstart :UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        //dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate = dateFormatter.stringFromDate(dateLabelstart.date)
+        StartDateFeild.text = strDate
+    }
+    
+    func datePickerChanged2(datePickerend :UIDatePicker) {
+        var dateFormatter1 = NSDateFormatter()
+        
+        dateFormatter1.dateStyle = NSDateFormatterStyle.ShortStyle
+        //dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate1 = dateFormatter1.stringFromDate(datePickerend.date)
+        EndDateFeild.text = strDate1
+    }
+    
+    func timePickerChanged1(ArrivalTimePicker :UIDatePicker) {
+        var dateFormatter1 = NSDateFormatter()
+        
+        //dateFormatter1.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter1.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate1 = dateFormatter1.stringFromDate(ArrivalTimePicker.date)
+        ArrivalFeild.text = strDate1
+    }
+    
+    func timePickerChanged2(DepartureTimePicker :UIDatePicker) {
+        var dateFormatter1 = NSDateFormatter()
+        
+        //dateFormatter1.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter1.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate1 = dateFormatter1.stringFromDate(ArrivalTimePicker.date)
+        DepartureFeild.text = strDate1
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
