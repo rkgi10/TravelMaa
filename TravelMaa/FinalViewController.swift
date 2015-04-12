6eaf2f3ad95f76b3e5e1a0b0de8dddddb0d76fc0
ykanani0 <ygkanani@gmail.com>
2015-04-12 06:35:50 +0530

    final view controller is been ready and a working ui!!!

---
+++ TravelMaa/FinalViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FinalViewController.swift
+//  TravelMaa
+//
+//  Created by yash kanani on 12/04/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func SetReminders(sender: UIButton) {
+    }
+    @IBAction func PsckSuitcase(sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TravelMaa/PurposeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PurposeViewController.swift
+//  TravelMaa
+//
+//  Created by yash kanani on 12/04/15.
+//  Copyright (c) 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class PurposeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func FamilyButton(sender: UIButton) {
+    }
+    @IBAction func BusinessButtton(sender: AnyObject) {
+    }
+    @IBAction func LoneButton(sender: UIButton) {
+    }
+    @IBAction func FriendsButton(sender: UIButton) {
+    }
+    @IBAction func RomanticButoon(sender: UIButton) {
+    }
+    @IBAction func EducationButton(sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
